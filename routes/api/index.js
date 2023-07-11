e70cc389d8e2eba49c49fdeda6a3d8086c3ee447
Courtney Sherman <csherman177@gmail.com>
2023-07-11 10:13:00 -0600

    pushing up new code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const userRoutes = require("./user");
+const userRoutes = require("./users");
 //const studentRoutes = require('./studentRoutes');
 
 router.use("/userRoutes", userRoutes);
